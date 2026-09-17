3a62ea1a3cd44870b94b6d27bdffe9e8ecc2249e
MatheusMiura-dn <matheusmiuradn@gmail.com>
2026-09-16 21:05:58 -0300

    fix: expose orders through client-safe server functions

--- src/lib/orders.functions.ts
+++ src/lib/orders.functions.ts
@@ -1,18 +1,19 @@
 import { createServerFn } from "@tanstack/react-start";
 import {
-  createOrder as createOrderServer,
-  listOrders as listOrdersServer,
-  findOrder as findOrderServer,
-  findPublicOrder as findPublicOrderServer,
-  updateOrderStatus as updateOrderStatusServer,
+  createOrderServer,
+  listOrdersServer,
+  findOrderServer,
+  findPublicOrderServer,
+  updateOrderStatusServer,
+  createOrderInput,
+  adminInput,
+  codeInput,
+  publicCodeInput,
+  updateOrderInput,
 } from "./orders.server";
 
-export const createOrder = createServerFn({ method: "POST" }).handler(async ({ data }) => createOrderServer({ data }));
-
-export const listOrders = createServerFn({ method: "GET" }).handler(async ({ data }) => listOrdersServer({ data }));
-
-export const findOrder = createServerFn({ method: "GET" }).handler(async ({ data }) => findOrderServer({ data }));
-
-export const findPublicOrder = createServerFn({ method: "GET" }).handler(async ({ data }) => findPublicOrderServer({ data }));
-
-export const updateOrderStatus = createServerFn({ method: "POST" }).handler(async ({ data }) => updateOrderStatusServer({ data }));
+export const createOrder = createServerFn({ method: "POST" }).validator(createOrderInput).handler(async ({ data }) => createOrderServer(data));
+export const listOrders = createServerFn({ method: "GET" }).validator(adminInput).handler(async ({ data }) => listOrdersServer(data));
+export const findOrder = createServerFn({ method: "GET" }).validator(codeInput).handler(async ({ data }) => findOrderServer(data));
+export const findPublicOrder = createServerFn({ method: "GET" }).validator(publicCodeInput).handler(async ({ data }) => findPublicOrderServer(data));
+export const updateOrderStatus = createServerFn({ method: "POST" }).validator(updateOrderInput).handler(async ({ data }) => updateOrderStatusServer(data));
